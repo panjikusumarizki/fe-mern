7c43dcac899748ee522607a4eb81cd8d29770ee1
Codmonk <pnksm009@gmail.com>
2021-05-04 11:21:58 +0700

    finishing component input date & breadcrumb

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react'
 
-import InputNumber from 'elements/Form/InputNumber'
+import { InputNumber, InputDate } from 'elements/Form'
+import Breadcrumb from 'elements/Breadcrumb'
 
 export default class Example extends Component {
-  state = {
-    value: '1'
-  }
+  // state = {
+  //   value: {
+  //     startDate: new Date(),
+  //     endDate: new Date(),
+  //     key: "selection",
+  //   },
+  // }
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+  // handleChange = e => {
+  //   this.setState({ [e.target.name]: e.target.value })
+  // }
 
   render() {
+    const breadcrumb = [
+      { pageTitle: 'Home', pageHref: '' },
+      { pageTitle: 'House Details', pageHref: '' }
+    ]
+
     return (
-      <div className='container'>
+      <div className="container">
         <div
          className="row align-items-center justify-content-center"
          style={{ height: '100vh' }}
         >
           <div className="col-auto">
-            <InputNumber 
+            <Breadcrumb data={breadcrumb}/>
+            {/* <InputDate
               max={30}
-              suffix=' night'
-              isSuffixPlural
               onChange={this.handleChange}
-              name='value'
+              name="value"
               value={this.state.value}
-            />
+            /> */}
           </div>
         </div>
       </div>
